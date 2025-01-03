a478e43381825d0138ffb8c84e8b3b90c9202c56
Jayce <captn.dev@protonmail.com>
2025-01-03 18:20:38 +0000

    basic prompting with model // requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# Quartermaster" 
+"# Quartermaster" 
---
+++ main.py
@@ -0,0 +1,27 @@
+import ollama
+
+assistant_convo = []
+
+def stream_response():
+    global assistant_convo
+    response_stream = ollama.chat(model='llama3.1:8b', messages=assistant_convo, stream=True)
+    complete_response = ''
+    print('QT:')
+
+    for chunk in response_stream:
+        print(chunk['message']['content'], end='', flush=True)
+        complete_response += chunk['message']['content']
+        
+    assistant_convo.append({'role': 'assistant', 'content': complete_response})
+    print('\n\n')
+
+def main():
+    global assistant_convo
+    
+    while True:
+        user_input = input('USER: \n')
+        assistant_convo.append({'role': 'user', 'content': user_input})
+        stream_response()
+    
+if __name__ == '__main__':
+    main()
