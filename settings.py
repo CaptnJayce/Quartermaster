2fcb61c52efca95867c5fe295ffa5b57b6b5cd0b
Jayce <captn.dev@protonmail.com>
2025-02-10 22:38:48 +0000

    added username setting

--- settings.py
+++ settings.py
@@ -11,31 +11,20 @@ def load_settings(filename="settings.json"):
             return json.load(file)
     return {}
 
-def assistant_name():
-    settings = load_settings()
+def user_name(settings):
+    print("What is your username?")
+    name = input("Enter: ")
+    settings['username'] = name
+
+def assistant_name(settings):
     print("What would you like your assistant to be called? (Leave blank for Quartermaster)")
     name = input("Enter: ")
-    settings['assistant_name'] = name if name else "Quartermaster"
-    save_settings(settings)
+    settings['assistant_name'] = name if name.strip() else "Quartermaster"
 
-def custom_prompt():
-    settings = load_settings()
+def custom_prompt(settings):
     print("Would you like to add a custom prompt? (Leave blank for no)")
-    prompt_check = input("Enter: ")
-    
-    if prompt_check.lower() != "yes":
-        settings['custom_prompt'] = False
-    else:
-        settings['custom_prompt'] = True
-    save_settings(settings)
+    settings['custom_prompt'] = input("Enter: ").lower() == "yes"
 
-def music_control():
-    settings = load_settings()
+def music_control(settings):
     print("Will you use Spotify for music? (Leave blank for no)")
-    music_check = input("Enter: ")
-    
-    if music_check.lower() != "yes":
-        settings['use_spotify'] = False
-    else:
-        settings['use_spotify'] = True
-    save_settings(settings)
+    settings['use_spotify'] = input("Enter: ").lower() == "yes"
