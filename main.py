891aa25c94628bb4d1cd71996ff079d259ada59c
Jayce <captn.dev@protonmail.com>
2025-02-10 22:39:26 +0000

    updated functions in main

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from audio import listen_with_timer
 from assistant import assistant
-from settings import assistant_name, custom_prompt, load_settings, music_control, save_settings
-import json
+from settings import user_name, assistant_name, custom_prompt, music_control, save_settings, load_settings
 
 if __name__ == "__main__":
     settings = load_settings()
-    if settings['first_launch'] == False:
-        assistant_name()
-        custom_prompt()
-        music_control()
-        settings['first_launch'] = True
-        save_settings(settings)
+    if settings.get('first_launch', True):
+        user_name(settings)
+        assistant_name(settings)
+        custom_prompt(settings)
+        music_control(settings)
 
+        settings['first_launch'] = False
+        save_settings(settings)
+    
     while True:
         query = listen_with_timer(timeout=5)
         if query:
