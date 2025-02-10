88d53919918d0417806a4b5fd88dbbd7c5e5a69d
Jayce <captn.dev@protonmail.com>
2025-02-10 21:52:47 +0000

    read/write settings

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from audio import listen_with_timer
 from assistant import assistant
-from settings import assistant_name, custom_prompt, music_control
+from settings import assistant_name, custom_prompt, load_settings, music_control, save_settings
+import json
 
 if __name__ == "__main__":
-    assistant_name()
-    custom_prompt()
-    music_control()
-      
+    settings = load_settings()
+    if settings['first_launch'] == False:
+        assistant_name()
+        custom_prompt()
+        music_control()
+        settings['first_launch'] = True
+        save_settings(settings)
+
     while True:
         query = listen_with_timer(timeout=5)
         if query:
--- settings.py
+++ settings.py
@@ -1,15 +1,41 @@
+import json
+import os
+
+def save_settings(settings, filename="settings.json"):
+    with open(filename, 'w') as file:
+        json.dump(settings, file, indent=4)
+
+def load_settings(filename="settings.json"):
+    if os.path.exists(filename):
+        with open(filename, 'r') as file:
+            return json.load(file)
+    return {}
+
 def assistant_name():
-    print("What would you like your assistant to be called?")
+    settings = load_settings()
+    print("What would you like your assistant to be called? (Leave blank for Quartermaster)")
     name = input("Enter: ")
+    settings['assistant_name'] = name if name else "Quartermaster"
+    save_settings(settings)
 
 def custom_prompt():
-    print("Would you like to add a prompt?")
+    settings = load_settings()
+    print("Would you like to add a custom prompt? (Leave blank for no)")
     prompt_check = input("Enter: ")
+    
     if prompt_check.lower() != "yes":
-        print("1")
+        settings['custom_prompt'] = False
+    else:
+        settings['custom_prompt'] = True
+    save_settings(settings)
 
 def music_control():
-    print("Will you use Spotify for music?")
+    settings = load_settings()
+    print("Will you use Spotify for music? (Leave blank for no)")
     music_check = input("Enter: ")
+    
     if music_check.lower() != "yes":
-        print("1")
+        settings['use_spotify'] = False
+    else:
+        settings['use_spotify'] = True
+    save_settings(settings)
